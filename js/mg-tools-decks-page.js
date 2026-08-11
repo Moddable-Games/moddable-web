8ac600e829c4477c4d5b6c816dfefb67f0f4a1cf
Mark Smalley <mark@smalley.my>
2026-08-11 16:14:39 +0100

    Add namespace anchor links + decks custom tab support (closes #10, #11)
    API page namespace cards now deep-link to anchored sections on
    tools.moddable.games. Decks page supports hash-based tab selection
    so /tools/decks/#custom links directly to the custom deck builder.

--- js/mg-tools-decks-page.js
+++ js/mg-tools-decks-page.js
@@ -3,8 +3,14 @@ import { tools } from './mg-tools-common.js'
 const panelEl = document.getElementById('decks-panel')
 
 if (panelEl) {
+  const defaultTab = window.location.hash.slice(1) || undefined
+
   tools.embed.decks(panelEl, {
+    tab: defaultTab,
     card: true,
     vars: { '--embed-bg': '#f5f4ef' },
+    onTab: (tab) => {
+      window.location.hash = tab
+    },
   })
 }
